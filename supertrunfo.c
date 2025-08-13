9c394e0319d06fcd6684a4b4adbaa70c9ce5cf8d
LuStaCo <202508027631@alunos.estacio.br>
2025-08-12 23:37:28 -0300

    ok

--- supertrunfo.c
+++ supertrunfo.c
@@ -2,24 +2,30 @@
 #include <stdio.h>
 	int main(){
 	//Variáveis carta 1
+	char est1[50];
 	char cid1[50];
+	char cod1={"A01","A02","A03","A04"};
 	int pop1;
 	float km1;
 	float pib1;
 	int tur1;
 	//Variáveis carta 2
+	char est2[50];
 	char cid2[50];
+	char cod2={"B01","B02","B03","B04"};
 	int pop2;
 	float km2;
 	float pib2;
 	int tur2;
 	//Identificação dos dados da carta 1
 	printf("#### Carta 1 ####\n");
-	printf("Estado: A\n");
-	printf("Código: A01\n");
+	printf("Estado: ");
+	fgets(est1, 50, stdin);
+
+	printf("Código: %s", cod1[1]);
 	
 	printf("Nome da cidade: ");
-	fgets(cid2, 50, stdin);	
+	fgets(cid1, 50, stdin);	
 	
 	printf("População total: ");
 	scanf("%i", &pop1);
@@ -35,8 +41,10 @@
 
 	//Identificação dos dados da carta 2
 	printf("#### Carta 2 ####\n");
-	printf("Estado: B\n");
-	printf("Código: B02\n");
+	printf("Estado: ");
+	fgets(est2, 50, stdin);
+
+	printf("Código: %s", B[1]);
 	
 	printf("Nome da cidade: ");
 	fgets(cid2, 50, stdin);
