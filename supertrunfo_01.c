1deb8b8afbe6e827e3c90e4b5e4f94c3fc21761f
LuStaCo <202508027631@alunos.estacio.br>
2025-08-19 03:45:46 +0000

    ok

--- supertrunfo_01.c
+++ supertrunfo_01.c
@@ -8,8 +8,6 @@
 	float km1;
 	float pib1;
 	int tur1;
-	float denspop1;
-	float pibcapta1;
 	//Variáveis carta 2
 	char est2[50];
 	char cid2[50];
@@ -18,8 +16,6 @@
 	float km2;
 	float pib2;
 	int tur2;
-	float denspop2;
-	float pibcapta2;
 	//Identificação dos dados da carta 1
 	printf("#### Carta 1 ####\n");
 	
@@ -32,46 +28,40 @@
 	fgets(cid2, 50, stdin);	
 	
 	printf("População total: ");
-	scanf("%i", &pop1);
+	scanf("%d", &pop1);
 	
 	printf("Área(km²): ");
 	scanf("%f", &km1);
 	
 	printf("PIB: ");
 	scanf("%f", &pib1);
-
+	
 	printf("Pontos turísticos: ");
-	scanf("%i", &tur1);
-
-	printf("Densidade populacional: %.2f\n", pop1/km1);
-
-	printf("PIB per capta: %.2f\n", pib1/pop1);
+	scanf("%d", &tur1);
 	
 	//Identificação dos dados da carta 2
 	printf("#### Carta 2 ####\n");
 	
 	printf("Estado: ");
+	//gets(est2, 50, stdin);
 	scanf("%s", &est2);
 
 	printf("Código: %s\n", cod2);
 	
 	printf("Nome da cidade: ");
+	//fgets(cid2, 50, stdin);
 	scanf("%s", &cid2);
 	
 	printf("População total: ");
-	scanf("%i", &pop2);
+	scanf("%d", &pop2);
 	
 	printf("Área(km²): ");
 	scanf("%f", &km2);
 	
 	printf("PIB: ");
 	scanf("%f", &pib2);
-
+	
 	printf("Pontos turísticos: ");
-	scanf("%i", &tur2);
-
-	printf("Densidade populacional: %.2f\n", pop2/km2);
-
-	printf("PIB per capta: %.2f\n", pib2/pop2);
+	scanf("%d", &tur2);
 return 0;
 }
