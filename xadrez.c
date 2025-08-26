13de73bd1aab100acd483ecca0c7f4a42afbd87c
LuStaCo <202508027631@alunos.estacio.br>
2025-08-26 03:59:58 +0000

    ok

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,19 @@ int main() {
     int j=1;
     int k=1;
 //Torre 5 casas para a direita
-printf("Movimentação da torre:\n");
+printf("#### Movimentação da torre ####\n");
     for(int i=0; i<5; i++){
         printf("Direita\n");
     }
 //Bispo 5 casas para a direita na diagonal
 printf("     ");
-printf("\nMovimentação do Bispo:\n");
+printf("\n#### Movimentação do Bispo ####\n");
     while(j<=5){
         printf("Cima, Direita\n");
     j++;
     }
 //Rainha 8 casas para a esquerda
-printf("\nMovimentação da Rainha\n");
+printf("\n#### Movimentação da Rainha ####\n");
     do{
         printf("Esquerda\n");
         k++;
