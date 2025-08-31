51068f943e25b53f256ac1dd4da5f5f120cc44d7
LuStaCo <202508027631@alunos.estacio.br>
2025-08-31 06:11:58 +0000

    ok

--- xadrez.c
+++ xadrez.c
@@ -1,25 +1,52 @@
 #include <stdio.h> 
+//Recursividade Torre
+void movetorre(int casa01){
+    if(casa01>0){
+        movetorre(casa01-1);
+        printf("%d casas à direita.\n", casa01);
+    }
+}
+
+//Recursividade Bispo
+void movebispo(int casa02){
+    for(int i=0; i<6; i++){
+        if(i>0 && i<6){
+            printf("Cima, diagonal;");
+        }
+        printf("\n");
+    }
+}
+
+
+
+//Recursividade Rainha
+void moverainha(int casa03){
+    if(casa03>0){
+        moverainha(casa03-1);
+        printf("%d casas à esquerda.\n", casa03);
+    }
+}
+
 int main() {
-//Indicadores de base    
-    int j=1;
-    int k=1;
-//Torre 5 casas para a direita
+    int pçtorre=5;
+    int pçbispo=5;
+    int pçrainha=8;
 printf("#### Movimentação da torre ####\n");
-    for(int i=0; i<5; i++){
-        printf("Direita\n");
-    }
-//Bispo 5 casas para a direita na diagonal
-printf("     ");
-printf("\n#### Movimentação do Bispo ####\n");
-    while(j<=5){
-        printf("Cima, Direita\n");
-    j++;
-    }
-//Rainha 8 casas para a esquerda
-printf("\n#### Movimentação da Rainha ####\n");
-    do{
-        printf("Esquerda\n");
-        k++;
-    } while(k<=5);
+    movetorre(pçtorre);
+    
+printf("\n#### Movimentação do bispo ####\n");
+movebispo(pçbispo);
+
+printf("\n#### Movimentação da rainha ####\n");
+    moverainha(pçrainha);
+
+printf("\n#### Movimentação do cavalo ####\n");
+    int i = 1;
+    do {
+        printf("Cima, externo.\n", i);
+        i++;
+    } while (i < 3);
+    printf("Direita, horizontal.\n");
+   
 return 0;
 }
